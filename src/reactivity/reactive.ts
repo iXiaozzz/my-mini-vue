73714bf425e494d672dacbf2d162a84a9cb08d2a
iXiaozzz <934170831@qq.com>
2022-08-09 23:04:08 +0800

    core: 删除node_modules文件夹

---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,13 @@
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      return res;
+    },
+
+    set(target, key, value) {
+      const res = Reflect.set(target, key, value)
+      return res
+    }
+  })
+}
